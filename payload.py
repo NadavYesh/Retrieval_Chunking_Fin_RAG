a2964124deb982440ec4fe15ea41243f333656c9
NadavYesh <nadavyesh1@gmail.com>
2026-06-08 17:45:14 +0200

    added classes script to avoid importing from an executable script. created initial collection with BM25 encoding. Automated the payload file to robustly and efficiently embed and upsert points using semantic vectors.

--- payload.py
+++ payload.py
@@ -1,37 +1,36 @@
 #%%
-import pydantic
+from classes import doc_payload
 import pandas as pd
 from qdrant_client import QdrantClient
-client = QdrantClient(url="http://localhost:6333", check_compatibility=True)
+client = QdrantClient(url="http://localhost:6333", check_compatibility=True, cloud_inference=False)
 import uuid
 from qdrant_client import models
+from qdrant_client.models import PointStruct
 from qdrant_client.models import Distance, VectorParams
 
 from typing import Optional
 from datetime import datetime
+
+import pickle
+from sentence_transformers import SentenceTransformer
+import torch
+
 #%%
-class doc_payload(pydantic.BaseModel):
-    '''
-    This class insures data consistency. Not all metadata fields exist for all chunks. Therefore, they're defaulted to None
-    '''
-    form_type: Optional[str] = None
-    company_name: Optional[str] = None
-    ticker: Optional[str] = None
-    fiscal_year_end: Optional[datetime] = None
-    section: Optional[str] = None
-    subsection: Optional[str] = None
-    item: Optional[str] = None
 
-    # Clean and normalize strings automatically
-    @pydantic.field_validator('form_type', 'company_name', 'ticker', 'section', 'subsection', 'item', mode="before")
-    @classmethod
-    def lowercase_string(cls, v):
-        if isinstance(v, str):
-            return v.strip().lower()
-        return v
 
-coll_name = "--split headers,chars --embeddings text-only"
+'''
+Available Collections:
+1. coll_name = "--split headers --embeddings text-only"
+2.coll_name = "--split headers,chars --embeddings text-only"
+3.coll_name = "--split headers --embeddings text,meta" ==> need to fixed repetition of word 'text'
+4.coll_name = "--split headers,chars --embeddings text,meta" ==> collection corrupted, run again
 
+
+'''
+
+
+coll_name = "--split headers,chars --embeddings text,meta"
+embed_meta = True
 
 
 
@@ -45,56 +44,60 @@ except Exception as e:
     print("exists")
 #%%
 client.create_payload_index(
-    collection_name="quant",
+    collection_name=coll_name,
     field_name="form_type",
     field_schema=models.PayloadSchemaType.KEYWORD, 
 )
 client.create_payload_index(
-collection_name="quant",
+collection_name=coll_name,
     field_name="company_name",
     field_schema=models.PayloadSchemaType.KEYWORD, 
 )
 client.create_payload_index(
-    collection_name="quant",
+    collection_name=coll_name,
     field_name="ticker",
     field_schema=models.PayloadSchemaType.KEYWORD, 
 )    
 client.create_payload_index(
-    collection_name="quant",
+    collection_name=coll_name,
     field_name="fiscal_year_end",
     field_schema=models.PayloadSchemaType.DATETIME, 
 )    
 client.create_payload_index(
-    collection_name="quant",
+    collection_name=coll_name,
     field_name="section",
     field_schema=models.PayloadSchemaType.TEXT, 
 )
 client.create_payload_index(
-    collection_name="quant",
+    collection_name=coll_name,
     field_name="subsection",
     field_schema=models.PayloadSchemaType.TEXT, 
 )
 client.create_payload_index(
-    collection_name="quant",
+    collection_name=coll_name,
     field_name="item",
     field_schema=models.PayloadSchemaType.TEXT, 
 )
-
+client.create_payload_index(
+    collection_name=coll_name,
+    field_name="text",
+    field_schema=models.TextIndexParams(
+        type=models.TextIndexType.TEXT,
+        tokenizer=models.TokenizerType.WORD,
+        lowercase=True,
+        phrase_matching=True,
+    ),
+)
 
 #%% upserting 
-# coll_name = "headers_split"
-import pickle
-from sentence_transformers import SentenceTransformer
-import torch
-from qdrant_client.models import PointStruct
 
 
 chunk_paths = [f"/Users/nadavsmacbookair/Desktop/Thesis/code/data/chunks/header_and_chars_split/{file}" for file in [
-    "3M_2018_10K.pkl" 
-    # "ADOBE_2016_10K.pkl",
-    # "PAYPAL_2022_10K.pkl", 
-    # "3M_2022_10K.pkl",
-    # "ADOBE_2017_10K.pkl"
+    "3M_2018_10K.pkl",
+    "ADOBE_2016_10K.pkl",
+    "PAYPAL_2022_10K.pkl", 
+    "3M_2022_10K.pkl",
+    "ADOBE_2017_10K.pkl"
     ]]
 
 # print number of chunks totally
@@ -131,7 +134,7 @@ for path in chunk_paths:
     
     texts = chunk['text'].tolist() if hasattr(chunk['text'], 'tolist') else chunk['text']
     metadatas = chunk['metadata'].tolist() if hasattr(chunk['metadata'], 'tolist') else chunk['metadata']
-    breakpoint()
+
     
     # Pre-process metadatas (date conversion)
     for i in range(len(metadatas)):
@@ -141,21 +144,22 @@ for path in chunk_paths:
             if not date_[-4:-2].isdigit() or int(date_[-4:-2]) < 19: # Simple heuristic for 2-digit year
                  date_ = date_[:len(date_)-2] + "20" + date_[len(date_)-2:]
             metadatas[i]["fiscal_year_end"] = datetime.strptime(date_, "%m-%d-%Y").date()
-
+    
+    if embed_meta: # concatenate the metadata with the text
+        combined = [str(metas)[1:len(str(metas))-1] + ", 'text': " + txt for (metas,txt) in zip(metadatas,texts)]
     # Process in sub-batches
     total_chunks = len(texts)
-    for batch_idx, (batch_texts, batch_metadatas) in enumerate(zip(get_batches(texts, batch_size), get_batches(metadatas, batch_size))):
+    for batch_idx, (batch_texts, batch_metadatas) in enumerate(zip(get_batches(combined if embed_meta else texts, batch_size), get_batches(metadatas, batch_size))):
         print(f"  Encoding batch {batch_idx + 1} ({len(batch_texts)} chunks)...")
         
         with torch.no_grad():
             embeddings = model.encode(batch_texts, show_progress_bar=False, batch_size=batch_size).tolist()
-
         points = []
         for idx, (metadata, text) in enumerate(zip(batch_metadatas, batch_texts)):
             try:
                 # Use doc_payload for validation and normalization
                 validated_payload = doc_payload(**metadata).model_dump()
-                validated_payload["text"] = text
+                validated_payload["text"] = text[text.index("'text':")+8:] if embed_meta else text
                 payload = validated_payload
             except Exception as e:
                 print(f"    Warning: Metadata validation failed for batch {batch_idx}, index {idx}: {e}")
@@ -186,11 +190,7 @@ for path in chunk_paths:
     if torch.backends.mps.is_available():
         torch.mps.empty_cache()
 
-
-#%% Inspect Collection
-print("Inspecting collection...")
-inspect_results = client.scroll(coll_name, limit=1000000)
-print(f"Retrieved {len(inspect_results[0])} points sample.")
+#%%
 
 # %%
 # if __name__ == "__main__":
@@ -215,7 +215,7 @@ print(f"Retrieved {len(inspect_results[0])} points sample.")
 #             print(f"Searching Qdrant for {name}...")
 #             for i, query_vector in enumerate(question_embeddings):
 #                 search_results = client.query_points(
-#                     collection_name="quant",
+#                     collection_name=coll_name,
 #                     query=query_vector.tolist(),
 #                     limit=5,
 #                     with_payload=True,
