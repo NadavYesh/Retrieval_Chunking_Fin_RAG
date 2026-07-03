15264e14ffa268a66e53840c8c35fdba1396f612
NadavYesh <nadavyesh1@gmail.com>
2026-07-03 21:32:54 +0200

    configered to html conversion

--- chunking/sec-pipeline.py
+++ chunking/sec-pipeline.py
@@ -245,8 +245,8 @@ def sec_chunking_pipeline_hierarchical(
 # ─────────────────────────────────────────────────────────────────────────────
 #%%
 if __name__ == "__main__":
-    RAW_DIR  = "/Users/nadavsmacbookair/Desktop/Thesis/data/html/indexed at 26-6-26/batch_2"
-    #RAW_FILES = [f for f in os.listdir(RAW_DIR) if f.endswith(".html")]
+    RAW_DIR  = '/Users/nadavsmacbookair/Desktop/Thesis/data/html/indexed at 03-07-26'
+    RAW_FILES = [f for f in os.listdir(RAW_DIR) if f.endswith(".html")]
 
     BUDGET = 400
     enc = tiktoken.encoding_for_model("text-embedding-3-small")
@@ -259,16 +259,16 @@ if __name__ == "__main__":
     )
 
     BASE_CHUNKS = "/Users/nadavsmacbookair/Desktop/Thesis/data/financial_corpora/chunks/hierarchical/indexed-at-30-06-26-limited-with-enriched"
-    BASE_MD     = "/Users/nadavsmacbookair/Desktop/Thesis/data/financial_corpora/md/indexed-at-26-06-26"
+    BASE_MD     = "/Users/nadavsmacbookair/Desktop/Thesis/data/financial_corpora/md/indexed-at-26-06-26/new_batch"
 
     # Load phi-4 once for the whole batch (expensive — skip if not running level 3)
     RUN_ENRICHED = True
-    RUN_FROM_MD = True # if we already have md.x
-    if RUN_FROM_MD: RAW_DIR = BASE_MD
-    TICKERS = ["WMT","NVDA"]
-    RAW_FILES = [f for f in os.listdir(RAW_DIR) if f.endswith(".md")]
-    if TICKERS:
-        RAW_FILES = [x for x in RAW_FILES if any(y in x for y in TICKERS)]
+    RUN_FROM_MD = False # if we already have md.x
+    # if RUN_FROM_MD: RAW_DIR = BASE_MD
+    # TICKERS = ["WMT","NVDA"]
+    # RAW_FILES = [f for f in os.listdir(RAW_DIR) if f.endswith(".md")]
+    # if TICKERS:
+    #     RAW_FILES = [x for x in RAW_FILES if any(y in x for y in TICKERS)]
 
     path_names = [os.path.join(RAW_DIR, f) for f in RAW_FILES] 
     caption_ = None
@@ -278,14 +278,15 @@ if __name__ == "__main__":
     #     caption_ = load("mlx-community/Llama-3.2-3B-Instruct-4bit")
 
     for (path, name) in zip(path_names, RAW_FILES):
-        name        = name[:-3] # make 5 for html
+        name        = name[:-5] # make 5 for html
         MD_PATH     = f"{BASE_MD}/{name}.md"
+        HTML_PATH     = f"{RAW_DIR}/{name}.html"
         doc_path    = f"{BASE_CHUNKS}/doc/{name}.pkl"
         header_path = f"{BASE_CHUNKS}/header/{name}.pkl"
         child_path  = f"{BASE_CHUNKS}/child/{name}.pkl"
         enriched_path = f"{BASE_CHUNKS}/enriched/{name}.pkl" if RUN_ENRICHED else None
         sec_chunking_pipeline_hierarchical(
-            html_path=None,
+            html_path=HTML_PATH,
             doc_path=doc_path,
             header_path=header_path,
             child_path=child_path,
